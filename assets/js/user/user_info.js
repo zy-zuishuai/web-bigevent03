4de7aa8721cdcc2c5e401c8f66b2321aad88e22a
itheima <itheim@itcast.cn>
2020-11-27 18:34:29 +0800

    个人中心功能完成

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,44 @@
+$(function(){
+    var form = layui.form;
+    form.verify({
+        nickname: function(value){
+            if(value.length > 6){
+                return '昵称长度为6 ~ 12位之间';
+            }
+        }
+    })
+    initUserInfo();
+    var layer = layui.layer;
+    function initUserInfo(){
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function(res){
+                // console.log(res);
+                if(res.status !==0){
+                   return layer.msg(res.message);
+                }
+                form.val('formUserInfo', res.data);
+            }
+        })
+    }
+    $("#btnReset").on('click', function(e){
+        e.preventDefault();
+        initUserInfo();
+    })
+    $('.layui-form').on('submit', function(e){
+        e.preventDefault();
+        $.ajax({
+            method: 'post',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function(res){
+                if(res.status !==0){
+                    return layer.msg('用户信息修改失败!')
+                }
+                layer.msg('恭喜您,用户信息修改成功!');
+                window.parent.getUserInfo();
+            }
+        })
+    })
+})
